d542a4c072db3a32fd63164a57c622753ef9927e
Robert Greeley <rgreeley@zappos.com>
2014-07-27 17:32:11 -0700

    An Important Message EX3

---
+++ FirstProg.java
@@ -0,0 +1,8 @@
+public class FirstProg
+{
+	public static void main( String[] args )
+	{
+		System.out.println( "Death is only the beginning!" );
+		System.out.println( "Today's date is July 27, 2014" );
+	}
+}
