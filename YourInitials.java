d7a31427007612e5dfe16a00ab590d8eedbbf107
Robert Greeley <rgreeley@zappos.com>
2014-07-27 20:23:25 -0700

    Your Initials EX6.2

--- YourInitials.java
+++ YourInitials.java
@@ -3,7 +3,7 @@ public class YourInitials
 	public static void main( String[] args )
 	{
 		System.out.println( "RRRR   CCC   GGG");
-		System.out.println(	"R   R C   C G   G");
+	    System.out.println(	"R   R C   C G   G");
 		System.out.println( "R   R C     G    ");
 		System.out.println( "RRRR  C     GGGGG");
 		System.out.println( "R R   C     G   G");
