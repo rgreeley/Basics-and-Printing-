94c4464540bf6dda52ab01f1d1bb4615fe7252c2
Robert Greeley <rgreeley@zappos.com>
2014-07-27 20:08:52 -0700

    A Letter to Yourself EX6

---
+++ LetterToYourself.java
@@ -0,0 +1,17 @@
+public class LetterToYourself 
+{
+	public static void main ( String[] args )
+	{
+		System.out.println( "+---------------------------------------------------------+" );
+		System.out.println( "|                                                    #### |" );
+		System.out.println( "|                                                    #### |" );
+		System.out.println( "|                                                    #### |" );
+		System.out.println( "|                                                         |" );
+		System.out.println( "|                                                         |" );
+		System.out.println( "|                             Robert Greeley              |" );
+		System.out.println( "|                             9153 Drifting Bay St.       |" );
+		System.out.println( "|                             Las Vegas, NV 89123         |" );
+		System.out.println( "|                                                         |" );
+		System.out.println( "+---------------------------------------------------------+" );
+	}
+}
